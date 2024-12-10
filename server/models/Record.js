d307fac566ead3cf13fd8c2bb9b4edaa85fa94c6
BASHISTHANAYAK <bashisthanayak09@gmail.com>
2024-12-11 01:22:37 +0530

    models created

--- server/models/Record.js
+++ server/models/Record.js
@@ -1,9 +1,22 @@
 const mongoose = require('mongoose');
 
 const RecordSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true },
-  age: { type: Number, required: true },
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true, // Enforce uniqueness
+    trim: true,
+    lowercase: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model('Record', RecordSchema);
